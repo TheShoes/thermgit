5c3c21a5d327a887cfba96a31ee07556fb849eb2
TheShoes <dkirschner43@gmail.com>
2025-07-25 17:49:21 -0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -49,7 +49,7 @@
     /* Manual positioning - uncomment and adjust as needed */
     position: relative;
     top: -10px;
-    left: 0px;
+    left: 10px;
     
     /* Or use margin for offset positioning */
     /* margin-top: 20px; */
